087d7f92afdaead928f3d0bc71e47a11a42fd61a
CalicoSpartan <thatcalicospartan@gmail.com>
2017-05-13 12:03:10 -0700

    Add files via upload

---
+++ ShopAndInventory.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __SHOPANDINVENTORY_H__
+#define __SHOPANDINVENTORY_H__
+
+#include "EngineMinimal.h"
+
+
+#endif
---
+++ ShopAndInventoryGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "ShopAndInventoryGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AShopAndInventoryGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AShopAndInventoryGameMode();
+};
+
+
+
